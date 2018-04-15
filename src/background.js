eb6dbe5fc925f3584428f0e64037b1efbfc23b79
Abdullah Diaa <abdullah@pushbots.com>
2018-04-15 03:20:33 +0200

    rename vars to use Youtube instead

--- src/background.js
+++ src/background.js
@@ -57,12 +57,11 @@ async function sendJailedDomainsToMAC () {
   try {
     return await browser.runtime.sendMessage(MAC_ADDON_ID, {
       method: "jailedDomains",
-      urls: FACEBOOK_DOMAINS.map((domain) => {
+      urls: YOUTUBE_DOMAINS.map((domain) => {
         return `https://${domain}/`;
       })
     });
   } catch (e) {
-    // We likely might want to handle this case: https://github.com/mozilla/contain-facebook/issues/113#issuecomment-380444165
     return false;
   }
 }
@@ -178,7 +177,7 @@ async function clearYouTubeCookies () {
         });
       });
       // Also clear Service Workers as it breaks detecting onBeforeRequest
-      await browser.browsingData.remove({hostnames: [facebookDomain]}, {serviceWorkers: true});
+      await browser.browsingData.remove({hostnames: [youtubeDomain]}, {serviceWorkers: true});
     });
   });
 }
